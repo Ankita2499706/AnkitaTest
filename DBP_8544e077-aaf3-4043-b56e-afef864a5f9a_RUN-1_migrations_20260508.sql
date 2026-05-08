d36b17c247037186b3b30774ad22dd2f0cecac9c
Ankita2499706 <ankita.chaudhari@fugetroncorp.com>
2026-05-08 11:57:56 +0530

    migration 7ab8bcaf-46aa-44b3-b36e-42363fb8cc98

---
+++ DBP_8544e077-aaf3-4043-b56e-afef864a5f9a_RUN-1_migrations_20260508.sql
@@ -0,0 +1,23 @@
+-- ============================================================
+-- MIGRATION SCRIPT
+-- ============================================================
+-- Target: PROD_DB.QUOTED_PROD
+-- ============================================================
+
+USE DATABASE PROD_DB;
+USE SCHEMA QUOTED_PROD;
+
+
+-- ------------------------------------------------------------
+-- DROP OBJECTS
+-- ------------------------------------------------------------
+
+DROP TABLE IF EXISTS "PROD_DB"."QUOTED_PROD"."ORDERS_LOWER";
+DROP TABLE IF EXISTS "PROD_DB"."QUOTED_PROD"."ORDER_DATA";
+DROP TABLE IF EXISTS "PROD_DB"."QUOTED_PROD"."ORDERS";
+DROP TABLE IF EXISTS "PROD_DB"."QUOTED_PROD"."orders_mixed";
+DROP TABLE IF EXISTS "PROD_DB"."QUOTED_PROD"."EXTRA_TABLE";
+
+-- ============================================================
+-- END - 5 statement(s)
+-- ============================================================
